335261486d505ee1b8e2ac59b1253b924c67477d
John Hawthorn <john.hawthorn@gmail.com>
2015-09-10 13:49:19 -0700

    Attempt to fix brain

--- start.sh
+++ start.sh
@@ -5,4 +5,7 @@ if [ -e ".hubotrc" ]; then
 	set +a
 fi
 
-exec bin/hubot $HUBOT_ARGS
+export PATH="node_modules/.bin:node_modules/hubot/node_modules/.bin:$PATH"
+export FILE_BRAIN_PATH="/var/lib/hubot"
+
+exec node_modules/.bin/hubot $HUBOT_ARGS
